41c63fb6b90bda9c88c5a184231b9b0a2d091845
Lucas <luquirueda01@gmail.com>
2026-02-15 23:22:09 -0300

    fix: update machine type in node pool configuration to 'e2-medium'

--- blockchain/infraestructura/terraform/terraform-cluster/nodepool_infra.tf
+++ blockchain/infraestructura/terraform/terraform-cluster/nodepool_infra.tf
@@ -5,7 +5,7 @@ resource "google_container_node_pool" "infra" {
   initial_node_count = var.infra_initial_nodes
 
   node_config {
-    machine_type = var.infra_machine_type
+    machine_type = e2-medium
 
     disk_type    = "pd-standard"
     disk_size_gb = 30
